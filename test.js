54cfeca2c91bd995a6bc843f1b1501e81ca86ffa
utku-guclu <utku_guclu@hotmail.com>
2024-03-07 20:33:40 +0300

    Update package version to v1.0.4

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const { mlToCups, cupsToMl } = require('./hydration-converter');
 
 test('Converts milliliters to cups correctly', () => {
-  expect(mlToCups(0)).toBe("You want to convert zero or negative milliliters? What a brilliant idea!");
+  expect(mlToCups(0)).toBe(0);
   expect(mlToCups(236.588)).toBe(1);
 });
 
 test('Converts cups to milliliters correctly', () => {
-  expect(cupsToMl(0)).toBe("Converting zero or negative cups? You must be a real optimist!");
+  expect(cupsToMl(0)).toBe(0);
   expect(cupsToMl(1)).toBe(237);
 });
